c4855f83a2b6703692127d70f865bbcaea6cd7e3
af482 <af482@af-works-1.myguest.virtualbox.org>
2024-11-20 17:30:06 -0500

    Implemented Game Area Handler in Backend. The handler determines what the command type is and will call the proper helper function. New functions for spectators has been implemented. Specs commands for the front end will be created in a seperate PR.

--- townService/src/town/games/BattleShipGameArea.ts
+++ townService/src/town/games/BattleShipGameArea.ts
@@ -1,16 +1,119 @@
+import { 
+  GAME_ID_MISSMATCH_MESSAGE, 
+  GAME_NOT_IN_PROGRESS_MESSAGE, 
+  INVALID_COMMAND_MESSAGE,
+} from '../../lib/InvalidParametersError';
 import Player from '../../lib/Player';
 import {
+  BattleShipMove,
+  GameMove,
+  GameMoveCommand,
   InteractableCommand,
   InteractableCommandReturnType,
   InteractableType,
+  JoinSpectatorCommand,
+  LeaveGameCommand,
+  LeaveSpectatorCommand,
 } from '../../types/CoveyTownSocket';
 import BattleShipGame from './BattleShipGame';
 import GameArea from './GameArea';
 
 export default class BattleShipGameArea extends GameArea<BattleShipGame> {
   // eslint-disable-next-line class-methods-use-this
+
+  playersInGame : Player[] = [];
+  observersInGame : Player[] = [];
+
   protected getType(): InteractableType {
     return 'BattleShipArea';
+  }
+
+  /**
+   * Deals with join game commands
+   * Helper Method handle command
+   * @param player The player who sent the command
+   */
+  public handleJoinCommand<CommandType extends InteractableCommand>(
+    player: Player,
+  ): InteractableCommandReturnType<CommandType> {
+    if (this._game === undefined) this._game = new BattleShipGame();
+    this._game.join(player);
+    this.playersInGame?.push(player);
+    this._emitAreaChanged();
+    return { gameID: this._game.id } as InteractableCommandReturnType<CommandType>;
+  }
+
+  /**
+   * Deals with Leave game commands
+   * Helper Method handle command
+   * @param command The sent command
+   * @param player The player who sent the command
+   */
+  public handleLeaveCommand<CommandType extends InteractableCommand>(
+    command: LeaveGameCommand,
+    player: Player,
+  ): InteractableCommandReturnType<CommandType> {
+    if(this._game === undefined) throw new Error(GAME_NOT_IN_PROGRESS_MESSAGE);
+    if(this._game.id !== command.gameID) throw new Error(GAME_ID_MISSMATCH_MESSAGE);
+    this._game.leave(player);
+    this._emitAreaChanged();
+    return undefined as InteractableCommandReturnType<CommandType>;
+  }
+
+  /**
+   * Deals with GameMove game commands
+   * Helper Method handle command
+   * @param command The sent command
+   * @param player The player who sent the command
+   */
+  public handleGameMoveCommand<CommandType extends InteractableCommand>(
+    command: GameMoveCommand<BattleShipMove>,
+    player: Player,
+  ): InteractableCommandReturnType<CommandType> {
+    if(this._game === undefined) throw new Error(GAME_NOT_IN_PROGRESS_MESSAGE);
+    if(this._game.id !== command.gameID) throw new Error(GAME_ID_MISSMATCH_MESSAGE);
+    this._game.applyMove({
+      gameID: command.gameID,
+      playerID: player.id,
+      move: command.move,
+    });
+    this._emitAreaChanged();
+    return undefined as InteractableCommandReturnType<CommandType>;
+  }
+  
+  /**
+   * Deals with Join Spectator commands
+   * Helper Method handle command
+   * @param command The sent command
+   * @param player The player who sent the command
+   */
+  handleSpectatorJoinCommand<CommandType extends InteractableCommand>(
+    command: JoinSpectatorCommand, 
+    player: Player
+  ): InteractableCommandReturnType<CommandType> {
+    if(this._game === undefined) throw new Error(GAME_NOT_IN_PROGRESS_MESSAGE);
+    if(this._game.id !== command.gameID) throw new Error(GAME_ID_MISSMATCH_MESSAGE);
+    this.observersInGame.push(player);
+    this._occupants.push(player);
+    this._emitAreaChanged();
+    return { gameID: this._game.id } as InteractableCommandReturnType<CommandType>;
+  }
+
+  /**
+   * Deals with Leave Spectator commands
+   * Helper Method handle command
+   * @param command The sent command
+   * @param player The player who sent the command
+   */
+  public handleSpectatorLeaveCommand<CommandType extends InteractableCommand>(
+    command: LeaveSpectatorCommand,
+    player: Player,
+  ): InteractableCommandReturnType<CommandType> {
+    if(this._game === undefined) throw new Error(GAME_NOT_IN_PROGRESS_MESSAGE);
+    if(this._game.id !== command.gameID) throw new Error(GAME_ID_MISSMATCH_MESSAGE);
+    this.observersInGame = this.observersInGame.filter(p => p.id !== player.id);
+    this._occupants = this.occupants.filter(p => p.id !== player.id);
+    return undefined as InteractableCommandReturnType<CommandType>;
   }
 
   /**
@@ -23,6 +126,11 @@ export default class BattleShipGameArea extends GameArea<BattleShipGame> {
     command: CommandType,
     player: Player,
   ): InteractableCommandReturnType<CommandType> {
-    throw new Error(`${this.id} ${command.type} ${player.id} Method not implemented.`);
+    if (command.type === 'JoinGame') return this.handleJoinCommand(player);
+    if (command.type === 'LeaveGame') return this.handleLeaveCommand(command,player);
+    if (command.type === 'GameMove') return this.handleGameMoveCommand(command,player);
+    if (command.type === 'JoinSpectator') return this.handleSpectatorJoinCommand(command,player);
+    if (command.type === 'LeaveSpectator') return this.handleSpectatorLeaveCommand(command,player);
+    throw new Error(INVALID_COMMAND_MESSAGE);
   }
 }
